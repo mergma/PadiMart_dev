374f1424651f73e631eb6c4e3fb8bd6686149c07
Biemers <bimasaktiindrajaya@gmail.com>
2025-12-02 09:58:13 +0700

    `Refactor CSS and PHP files with various theme-aware and mobile enhancements`

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $userName = $isLoggedIn ? $_SESSION['user_name'] : '';
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>PADI MART</title>wwwww
+    <title>PADI MART</title>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
